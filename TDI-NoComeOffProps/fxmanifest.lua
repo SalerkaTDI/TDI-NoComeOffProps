49c11a8128f25443814ae9f46e1d2f48f3ac326b
SalerkaTDI <autoveidas@gmail.com>
2025-02-02 11:17:08 +0200

    Add files via upload

---
+++ TDI-NoComeOffProps/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'SalerkaTDI'
+description 'Script that hats/props dosent come off when punched '
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
